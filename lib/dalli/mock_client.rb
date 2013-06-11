a58f629a56e3f916ffc6ebac8608ba16be12d50f
tlemburg <trlemburg@gmail.com>
2013-06-11 15:55:51 -0500

    Multi, cas, incr, decr methods

--- lib/dalli/mock_client.rb
+++ lib/dalli/mock_client.rb
@@ -6,16 +6,30 @@ module Dalli
       @expiries = {}
     end
 
+    def multi
+      yield
+    end
+
+    def cas(key, ttl = nil, options = nil, &block)
+      ttl ||= @options[:expires_in].to_i
+      value = get(key)
+      if value
+        newvalue = block.call(value)
+        set(key, newvalue, ttl, options)
+      end
+    end
+
     def get_multi(*keys)
       values = {}
       options = keys.pop if keys.last.is_a?(Hash) || keys.last.nil?
+      options ||= {}
       keys.each do |key|
         values[key] = get(key, options)
       end
       values
     end
 
-    def get(key, options = nil)
+    def get(key)
       if @expiries[key].nil? || @expiries[key] > Time.now.to_i
         return @data[key]
       else
@@ -36,6 +50,38 @@ module Dalli
       @data[key] = value
       @expiries[key] = Time.now.to_i + ttl if ttl != 0
       true
+    end
+
+    def incr(key, amt = 1, ttl = nil, default = nil)
+      if !amt.respond_to? :to_i || amt < 0
+        raise ArgumentError, "Positive values only: #{amt}"
+      end
+      ttl ||= @options[:expires_in].to_i
+      value = get(key)
+      unless value.nil? value.is_a? Integer && value > 0
+        raise ArgumentError, "Key #{key} does not have a positive integer value"
+      if value.nil?
+        value = default
+      else
+        value += amt.to_i
+      end
+      set(key, value, ttl)
+    end
+
+    def decr(key, amt = 1, ttl = nil, default = nil)
+      if !amt.respond_to? :to_i || amt < 0
+        raise ArgumentError, "Positive values only: #{amt}"
+      end
+      ttl ||= @options[:expires_in].to_i
+      value = get(key)
+      unless value.nil? value.is_a? Integer && value > 0
+        raise ArgumentError, "Key #{key} does not have a positive integer value"
+      if value.nil?
+        value = default
+      else
+        value -= amt.to_i
+      end
+      set(key, value, ttl)
     end
 
     def delete(key)
